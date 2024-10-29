c1703d6e95d186d8404c6fa8a577572bdc75ef8b
Wrdouuu <164611150+Wrdouuu@users.noreply.github.com>
2024-10-29 20:33:59 +0700

    m

---
+++ Assets/UI Manager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text Health;
+    public Text Laps;
+
+
+
+}
